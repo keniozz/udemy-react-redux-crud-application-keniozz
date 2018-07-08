d2fd7c0cd9a121bc261fdbed17f8da4b34773f23
keniozz <fujitani.janakute.fujiya.datte@gmail.com>
2018-07-08 21:28:44 +0900

    create action

--- src/App.js
+++ src/App.js
@@ -4,27 +4,27 @@ const App = ()=>(<Counter>    </Counter>);
 
 class Counter extends Component{
   constructor(props){
-    super(props)
-    this.state = { count : 0}
+    super(props);
+    this.state = { count : 0};
   }
 
   handlePlusButton=()=>{
-    this.setState({ count : this.state.count + 1})
+    this.setState({ count : this.state.count + 1});
   }
 
   handleMinusButton=()=>{
-    this.setState({ count : this.state.count - 1})
+    this.setState({ count : this.state.count - 1});
   }
 
   render(){
-    console.log("render")
+    console.log("render");
     return (
       <React.Fragment>
         <div> count: {this.state.count} </div>
         <button onClick={this.handlePlusButton} >+1</button>
         <button onClick={this.handleMinusButton} >-1</button>
       </React.Fragment>
-    )
+    );
   }
 }
 
